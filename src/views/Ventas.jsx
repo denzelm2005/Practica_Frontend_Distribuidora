300a4f03766a228e0be08a2172a39373e39788bd
Denzelm2005 <denzelm645@gmail.com>
2025-04-28 14:17:50 -0600

    Se agrego nueva funcion en ventas

--- src/views/Ventas.jsx
+++ src/views/Ventas.jsx
@@ -1,6 +1,7 @@
 // Importaciones necesarias para la vista
 import React, { useState, useEffect } from 'react';
 import TablaVentas from '../components/ventas/TablaVentas'; // Importa el componente de tabla
+import ModalDetallesVenta from '../components/detalles_ventas/ModalDetallesVentas';
 import { Container } from "react-bootstrap";
 
 // Declaración del componente Ventas
@@ -10,11 +11,34 @@ const Ventas = () => {
   const [cargando, setCargando] = useState(true);     // Controla el estado de carga
   const [errorCarga, setErrorCarga] = useState(null); // Maneja errores de la petición
 
+  const [mostrarModal, setMostrarModal] = useState(false); // Estado para el modal
+  const [detallesVenta, setDetallesVenta] = useState([]); // Estado para los detalles
+  const [cargandoDetalles, setCargandoDetalles] = useState(false); // Estado de carga de detalles
+  const [errorDetalles, setErrorDetalles] = useState(null); // Estado de error de detalles
+
+  // Función para obtener detalles de una venta
+  const obtenerDetalles = async (id_venta) => {
+    setCargandoDetalles(true);
+    setErrorDetalles(null);
+    try {
+      const respuesta = await fetch(`http://localhost:3000/api/obtenerdetallesventa/${id_venta}`);
+      if (!respuesta.ok) {
+        throw new Error('Error al cargar los detalles de la venta');
+      }
+      const datos = await respuesta.json();
+      setDetallesVenta(datos);
+      setCargandoDetalles(false);
+      setMostrarModal(true); // Abre el modal
+    } catch (error) {
+      setErrorDetalles(error.message);
+      setCargandoDetalles(false);
+    }
+  };
   // Lógica de obtención de datos con useEffect
   useEffect(() => {
     const obtenerVentas = async () => {
       try {
-        const respuesta = await fetch('http://localhost:3000/api/ventas'); // Ruta ajustada al controlador
+        const respuesta = await fetch('http://localhost:3000/api/obtenerventas'); // Ruta ajustada al controlador
         if (!respuesta.ok) {
           throw new Error('Error al cargar las ventas');
         }
@@ -41,7 +65,18 @@ const Ventas = () => {
           ventas={listaVentas}
           cargando={cargando}
           error={errorCarga}
+          obtenerDetalles={obtenerDetalles} // Pasar la función
         />
+
+        <ModalDetallesVenta
+          mostrarModal={mostrarModal}
+          setMostrarModal={setMostrarModal}
+          detalles={detallesVenta}
+          cargandoDetalles={cargandoDetalles}
+          errorDetalles={errorDetalles}
+        />
+
+
       </Container>
     </>
   );

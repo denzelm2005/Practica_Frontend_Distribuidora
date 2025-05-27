3378a48f3dc2913b761168560fbc0f1b419f9923
Denzelm2005 <denzelm645@gmail.com>
2025-05-27 14:11:10 -0600

    Se agregan nuevos reportes en estadisticas

--- src/components/graficos/VentasPorEmpleado.jsx
+++ src/components/graficos/VentasPorEmpleado.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import { Card } from 'react-bootstrap';
+import { Card, Button } from 'react-bootstrap';
 import { Bar } from 'react-chartjs-2';
 import Chart from 'chart.js/auto';
-
+import { useRef } from "react";
+import jsPDF from "jspdf";
+import autoTable from "jspdf-autotable";
 
 const VentasPorEmpleado = ({ empleados, totales_por_empleado }) => {
   // Define chart data
@@ -58,14 +60,61 @@ const VentasPorEmpleado = ({ empleados, totales_por_empleado }) => {
     },
   };
 
+  const chartRef = useRef(null);
+
+  const generarPDF = () => {
+    const doc = new jsPDF();
+
+    // Encabezado
+    doc.setFillColor(28, 41, 51);
+    doc.rect(0, 0, doc.internal.pageSize.getWidth(), 30, "F");
+    doc.setTextColor(255, 255, 255);
+    doc.setFontSize(22);
+    doc.text("Reporte de Ventas por Empleado", doc.internal.pageSize.getWidth() / 2, 20, { align: "center" });
+
+    // Capturar gráfico como imagen
+    const chartInstance = chartRef.current;
+    const chartCanvas = chartInstance?.canvas;
+    const chartImage = chartCanvas?.toDataURL("image/png", 1.0);
+
+    if (chartImage) {
+      doc.addImage(chartImage, "PNG", 14, 40, 180, 100);
+    }
+
+    // Tabla de datos
+    const columnas = ["Empleado", "Ventas (C$)"];
+    const filas = empleados.map((empleado, index) => [empleado, totales_por_empleado[index]]);
+
+    autoTable(doc, {
+      head: [columnas],
+      body: filas,
+      startY: 150,
+      theme: "grid",
+      styles: { fontSize: 10, cellPadding: 2 },
+      margin: { top: 20, left: 14, right: 14 },
+    });
+
+    // Generar un nombre dinámico para el archivo PDF
+    const fecha = new Date();
+    const dia = String(fecha.getDate()).padStart(2, '0');
+    const mes = String(fecha.getMonth() + 1).padStart(2, '0');
+    const anio = fecha.getFullYear();
+    const nombreArchivo = `VentasPorEmpleado_${dia}${mes}${anio}.pdf`;
+
+    // Guardar PDF
+    doc.save(nombreArchivo);
+  };
+
   return (
     <Card>
-      <Card.Body>
         <Card.Title>Ventas por Empleado</Card.Title>
         <div style={{ height: "300px", justifyContent: "center", alignItems: "center", display: "flex" }}>
-        <Bar data={data} options={options} />
-    </div>
-      </Card.Body>
+          <Bar ref={chartRef} data={data} options={options} />
+        </div>
+        <Button className="btn btn-primary mt-3" onClick={generarPDF}>
+          Generar Reporte <i className="bi bi-download"></i>
+        </Button>
+      
     </Card>
   );
 };
